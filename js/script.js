78643cfd281a14e437847b5d062d97ee43677696
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-05-24 01:53:05 +0000

    added image, changed css and edited html and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// script.js
+
+// 
+function pastaOrder() {
+    let pastatype = document.getElementById("pastaType").value
+    let 
+}
